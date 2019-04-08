e5ed077873f4da2029f379d3d2e1e20f3992cd63
mipusi.de <48703892+mipuside@users.noreply.github.com>
2019-04-08 08:13:34 +0700

    Add files via upload

---
+++ Selection Sort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int data[4] = {3, 4, 1, 2};
+				// 0  1  2  3 = indeks
+				
+
+	for(int i=0; i<3; i++){
+		int temp;
+		int indeks = i;
+		
+		for(int j=i+1; j<=3; j++){
+			if(data[indeks]>data[j]){
+				indeks = j; 
+			}
+		}
+		
+		temp = data[indeks];
+		data[indeks] = data[i];
+		data[i] = temp; //cari tau isi arraynya
+	}
+	for(int i = 0; i<4; i++){
+		cout << data[i] << " ";
+	}
+}
